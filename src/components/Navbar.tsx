5ac2d634ce6c57f79499136468dc2577e6640989
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-22 14:36:38 -0300

    feat: navbar links added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import '../styles/navbar.scss'
+
 export function Navbar(){
   return(
-    <>
-      <h3>Navbar</h3>
-    </>
+    <header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/profile">My Profile</Link>
+        </li>
+      </ul>
+    </header>
   )
 }
