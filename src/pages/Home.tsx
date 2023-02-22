9a5b497a2752e3a8e992f4b7c6a70c1744c15b5d
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-22 11:31:00 -0300

    feat: Home components added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "../components/Navbar";
+import { Store } from "../components/Store";
+
+export function Home(){
+  return(
+    <>
+      <Navbar />
+      <Store />
+    </>
+  )
+}
