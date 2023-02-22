702f74287ff48c09a5a1d41000b5fcebe80e5c88
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-22 14:35:58 -0300

    feat: Cart and profile components added

---
+++ src/components/Cart.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "./Navbar";
+
+export function Cart(){
+  return(
+    <>
+      <Navbar />
+      <h1>Cart</h1>
+    </>
+  )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from "./Navbar";
+
+export function Profile(){
+  return(
+    <>
+      <Navbar />
+      <h1>Profile</h1>
+    </>
+  )
+}
