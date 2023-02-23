4a25145a654831f7ba79b07e53eb3d506c622ecd
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-02-23 17:04:52 -0300

    feat: shopping card and functionalities added

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import { api } from '../lib/axios'
 import '../styles/store.scss'
+import {BsFillCartPlusFill, BsCartCheckFill} from 'react-icons/bs'
 
 export function Store(){
-  const [cellphones, setCellphones] = useState<any>([])
+  const [cellphones, setCellphones] = useState([])
+  const [cart, setCart] = useState([])
 
   async function fetchCellphones(){
     const response = await api.get('search?q=celular')
@@ -14,6 +16,17 @@ export function Store(){
     fetchCellphones()
   }, [])
 
+  function handleAddCartButton(obj:any){
+    const element = cart.find((e) => e.id === obj.id)
+
+    if(element){
+      const arrFilmer = cart.filter((e) => e.id !== obj.id)
+      setCart(arrFilmer)
+    }else{
+      setCart([...cart, obj])
+    }
+  }
+
   return(
     <div className="store">
       {cellphones.map((cellphone:any, i:any) => {
@@ -22,6 +35,15 @@ export function Store(){
             <h5>{cellphone.title}</h5>
             <img src={cellphone.thumbnail} alt={cellphone.title} />
             <p><strong>R$ {cellphone.price}</strong></p>
+            <button onClick={() => handleAddCartButton(cellphone)}>
+              {
+                cart.some((itemCart) => itemCart.id === cellphone.id) ? (
+                  <BsCartCheckFill  size={30}/>
+                ) : (
+                  <BsFillCartPlusFill size={30} />
+                )
+              }
+            </button>
           </div>
         )
       })}
